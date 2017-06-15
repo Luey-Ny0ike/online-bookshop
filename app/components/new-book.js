af35dc7d5f97e6ec4739d33b2f67e67235831ea3
lewis nyoike <nyoikelewis@gmail.com>
2017-06-15 10:32:56 +0300

    edited js

--- app/components/new-book.js
+++ app/components/new-book.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewBook: false;
+  actions: {
+    bookFormShow() {
+      this.set('addNewBook', true);
+    },
+
+    
+  }
 });
